2de4ebb39ab4eac9884ecd6d149b36d80aa139a4
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-28 14:47:49 -0800

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'static/index'
   namespace :v1, defaults: { format: 'json' } do
     get 'sectors', to: 'sectors#index'
   end
 
-  root 'static#index'
+  root 'sectors#index'
 
 end
