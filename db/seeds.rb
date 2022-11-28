8197e36f7b4a8a17f8f63edc6dc3718d7d8c1231
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-28 14:48:38 -0800

    Setup database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    Sector.create([{ name: "Manufaturing"}, { name: "Construction materials"}, { name: "Electronics and Optics"},
+        , { name: "Food and Beverage"}, { name: "Bakery and confectionery products"}, { name: "Beverages"}, { name: "Fish & Fish products"},
+        , { name: "Meat & meat products"}, { name: "Milk & diary products"}, { name: "Other"}, { name: "Sweets & snack food"},
+        , { name: "Furniture"}, { name: "Bathroom/sauna"}, { name: "Bedroom"}, { name: "Chiildren's room"}, { name: "Kitchen"},
+        , { name: "Living room"}, { name: "Office"}, { name: "Other(Furniture)"}, { name: "Outdoor"}, { name: "Project furniture"},
+        , { name: "Machinery"}, { name: "Machinery components"},{ name: "Machinery equipment/tools"}, { name: "Manufacture of machinery"},
+        , { name: "Maritime"}, { name: "Aluminium and steel workboats"}, { name: "Boat/Yacht building"}, { name: "Ship repair and conversion"},
+        , { name: "Metal structures"}, { name: "Other"}, { name: "Repair and maintenance service"}, { name: "Metalworking"},
+        , { name: "Construction of metal structures"}, { name: "Houses and buildings"}, { name: "Metal products"}, { name: "Metal works"},
+        , { name: "CNC-machining"}, { name: "Forgings,Fasteners"}, { name: "Gas,Plasma, Laser"}, { name: "MIG, TIG, Aluminium"},
+        , { name: "Plastic and Rubber"}, { name: "Packaging"}, { name: "Plastic goods"}, { name: "Plastic processing"},
+        , { name: "Blowing"}, { name: "Moulding"}, { name: "Plastics welding and processing"}, { name: "Plastic profiles"},
+        , { name: "Printing"}, { name: "Advertising"}, { name: "Book/Periodicals printing"}, { name: "Labelling and packaging printing"},
+        , { name: "Textile and clothing"}, { name: "Clothing"}, { name: "Textile"}, { name: "Wood"}, { name: "Other(Wood)"}, { name: "Wooden building materials"},
+        { name: "Wooden houses"}, { name: "Other"}, { name: "Creative industries"}, { name: "Energy technology"}, { name: "Environment"}, { name: "Service"},
+        { name: "Environment"}, { name: "Service"}, { name: "Business services"}, { name: "Engineering"}, { name: "Information Technology and Telecommunications"}, { name: "Data processing, Web portals, E-marketing"},
+        { name: "Programming,Consultancy"}, { name: "Software,Hardware"}, { name: "Telecommunications"}, { name: "Tourism"}, { name: "Translation services"}, { name: "Transport and Logistics"},
+        { name: "Air"}, { name: "Rail"}, { name: "Road"}, { name: "Water"},
+       ])
