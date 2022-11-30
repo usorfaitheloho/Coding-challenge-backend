d4b645157e197e4c910fb1675bdbab3317f0e29e
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-29 18:28:52 -0800

    Fix linters

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,5 +1,5 @@
 class Sector < ApplicationRecord
-    validates :user_name, presence: true, length: { maximum: 30 }
-    validates :sector_name, presence: true
-    validates :agree, presence: true
+  validates :user_name, presence: true, length: { maximum: 30 }
+  validates :sector_name, presence: true
+  validates :agree, presence: true
 end
