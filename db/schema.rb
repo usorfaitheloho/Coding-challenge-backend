1f42c511450c4c7bb65243318793a48f5c0a83bd
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-29 18:04:35 -0800

    Add migration data

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_210104) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_012052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "sectors", force: :cascade do |t|
     t.string "sector_name"
-    t.string "user_name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sectors_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.boolean "agree"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "sectors", "users"
 end
