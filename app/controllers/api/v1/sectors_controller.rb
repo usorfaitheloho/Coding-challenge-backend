4ed3e2f36538d7279ba9372d23040e09f9f5d063
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-30 16:36:53 -0800

    Modified Sectors controller

--- app/controllers/api/v1/sectors_controller.rb
+++ app/controllers/api/v1/sectors_controller.rb
@@ -3,33 +3,12 @@ class Api::V1::SectorsController < ApplicationController
         @sectors = Sector.all
         render json: {sectors: @sectors }.to_json,status: :ok 
     end
-
-    def create
-        @sector = Sector.new(sector_params)
-
-        if @sector.save
-            render json: {
-                message: 'Details added successfully'
-            }, status: :ok
-
-        else
-            render json: {
-                message: 'Something went wrong'
-            }, status: :unprocessable_entity
-        end
-    end
-
-    def show
-        @sector = Sector.find(params[:id])
-        render json: {sector: @sector}.to_json, status: :created
-    end
+  
 
     def update
         @sector = Sector.find(params[:id])
         @sector.update(
             sector_name: params[:sector_name],
-            user_name: params[:user_name],
-            agree: params[:agree]
         )
         render json: @sector
     end
@@ -42,7 +21,7 @@ class Api::V1::SectorsController < ApplicationController
     end
     
     def sector_params
-        params.require(:sector).permit(:sector_name, :user_name, :agree)
+        params.require(:sector).permit(:sector_name)
     end
 
 end
