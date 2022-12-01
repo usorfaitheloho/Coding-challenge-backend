e01731fe090ede144b163afb1078a0f6586d926e
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-30 16:37:58 -0800

    Modified Sector's model

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -1,6 +1,3 @@
 class Sector < ApplicationRecord
-    belongs_to :user, class_name: 'User'
-
-    validates_presence_of :user
-    validates :name, presence: true, length: {maximum: 30 }
+     validates :sector_name, presence: true
 end
