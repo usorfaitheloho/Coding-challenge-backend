236282c82d3158cc9464fb22322ccbef719543cf
usorfaitheloho <usorfaitheloho@gmail.com>
2022-11-30 16:39:54 -0800

    Modified Users and Sectors table

--- db/migrate/20221130012052_create_sectors.rb
+++ db/migrate/20221130012052_create_sectors.rb
@@ -2,7 +2,6 @@ class CreateSectors < ActiveRecord::Migration[7.0]
   def change
     create_table :sectors do |t|
       t.string :sector_name
-      t.references :user, index: true,  foreign_key: { to_table: :users}
 
       t.timestamps
     end
