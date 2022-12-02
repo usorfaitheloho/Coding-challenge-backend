8b01fc0ef1307d025c3e9834ea2210adb78afbb6
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-02 13:55:55 -0800

    Add sectors and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :sectors
-    get 'sectors', to: 'sectors#index'
+      resources :users
     end
   end
 
