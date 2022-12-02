fc4a32f70014456b8646c3c268b909232600e021
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-02 14:22:21 -0800

    Fix linter error

--- app/controllers/sectors_controller.rb
+++ app/controllers/sectors_controller.rb
@@ -1,34 +1,37 @@
 class SectorsController < ApplicationController
-    def index
-        @sectors = Sector.all
-        render json: @sectors
-    end
-    def show
-        @sector = Sectors.find(params[:id])
-        render json: @sector
-    end
-    def create
-        @sector = Sector.create(
-            sector_name: params[:sector_name],
-            user_name: params[:user_name],
-            agree: params[:agree]
-        )
-        render json: @sector
-    end
-    def update
-        @sector = Sector.find(params[:id])
-        @sector.update(
-            sector_name: params[:sector_name],
-            user_name: params[:user_name],
-            agree: params[:agree]
-        )
-        render json: @sector
-    end
-    def destroy
-        @sectors = Sector.all
-        @sector = Sector.find(params[:id])
-        @sector.destroy
-        render json: @sectors
-    end
+  def index
+    @sectors = Sector.all
+    render json: @sectors
+  end
 
+  def show
+    @sector = Sectors.find(params[:id])
+    render json: @sector
+  end
+
+  def create
+    @sector = Sector.create(
+      sector_name: params[:sector_name],
+      user_name: params[:user_name],
+      agree: params[:agree]
+    )
+    render json: @sector
+  end
+
+  def update
+    @sector = Sector.find(params[:id])
+    @sector.update(
+      sector_name: params[:sector_name],
+      user_name: params[:user_name],
+      agree: params[:agree]
+    )
+    render json: @sector
+  end
+
+  def destroy
+    @sectors = Sector.all
+    @sector = Sector.find(params[:id])
+    @sector.destroy
+    render json: @sectors
+  end
 end
