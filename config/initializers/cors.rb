1f86c06e3e86a4c5878d400a2c928b58ac2c74d1
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-06 12:20:42 -0800

    Add swagger files automatically

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do 
-    origins ["http://localhost:3000"]
+    origins ["http://localhost:3000","http://localhost:3001" ]
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      expose: ["Authorization", "access-token","expiry","client","uuid","token-type"],
+      max_age: 600
   end
 end
