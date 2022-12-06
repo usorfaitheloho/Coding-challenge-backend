811dd410d29aaee807f6c0b32200125460bc1599
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-06 14:09:37 -0800

    Add devise to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 30 }
   validates :sector_name, presence: true
   validates :agree, presence: true
