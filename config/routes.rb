a8d57964ef378319e2b96598b1c8d05b20a1e1a2
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-10 12:28:33 -0800

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  scope :api, defaults: { format: 'json' } do
+    devise_for :admin, 
+    controllers: { 
+      registrations: 'admin/registrations',
+      sessions: 'admin/sessions' }
+end
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
