2561b7efaab4f16b74481aa53d86ff6d973f7289
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-12 20:45:43 -0800

    Add registrations controller actions

--- app/controllers/admin/registrations_controller.rb
+++ app/controllers/admin/registrations_controller.rb
@@ -4,46 +4,6 @@ class Admin::RegistrationsController < Devise::RegistrationsController
 
   respond_to :json
  
-
-  # private
-
-  
-  # def respond_with(_resource, _opts = {})
-  #   register_success && return if resource.persisted?
-
-  #   register_failed
-  # end
-
-  # def register_success
-  #   render json: {
-  #     message: 'Signed up successfully',
-  #     admin: current_admin,
-  #     status: 200
-  #   }, status: :ok
-  # end
-
-  # def register_failed
-  #   render json: {
-  #     message: 'Somehing went wrong',
-  #     status: 401
-  #   }, status: :unprocessable_entity
-  # end
-
-  # def register_success
-  #   render json: {
-  #     message: 'Signed up successfully',
-  #     user: current_user,
-  #     status: 200
-  #   }, status: :ok
-  # end
-
-  # def register_failed
-  #   render json: {
-  #     message: 'Somehing went wrong',
-  #     status: 401
-  #   }, status: :unprocessable_entity
-  # end
-
   def new
     @admin = current_admin[:admin_id]
   end
