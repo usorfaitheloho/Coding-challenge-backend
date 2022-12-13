90b96e658100a0a9881532131c446ea6c5d3c6f5
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-12 21:08:43 -0800

    Add Admin table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_164517) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_11_181548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,12 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_164517) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "jwt_allowlistedjwts", force: :cascade do |t|
+  create_table "allowlistedjwts", force: :cascade do |t|
     t.string "jti"
-    t.datetime "exp"
+    t.string "aud"
+    t.datetime "exp", null: false
+    t.bigint "admin_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["jti"], name: "index_jwt_allowlistedjwts_on_jti"
+    t.index ["admin_id"], name: "index_allowlistedjwts_on_admin_id"
+    t.index ["jti"], name: "index_allowlistedjwts_on_jti"
   end
 
   create_table "sectors", force: :cascade do |t|
@@ -48,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_164517) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "allowlistedjwts", "admins", on_delete: :cascade
 end
