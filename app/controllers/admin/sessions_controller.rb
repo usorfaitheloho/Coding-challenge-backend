89275fab89a4ea4e36b7a7ecbfd517053943db91
usorfaitheloho <usorfaitheloho@gmail.com>
2022-12-12 20:48:56 -0800

    Add session controller

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,54 +1,6 @@
-# frozen_string_literal: true
-
 class Admin::SessionsController < Devise::SessionsController
   respond_to :json 
-  private
-
-  # def respond_with(_resource, _opts = {})
-  #   login_success && return if current_user
-
-  #   login_failed
-  # end
-
-  # def login_success
-  #   render json: {
-  #     message: 'You are logged In',
-  #     user: current_user,
-  #     token: request.env['warden-jwt_auth.token'],
-  #     status: 200
-  #   }, status: :ok
-  # end
-
-  # def login_failed
-  #   render json: {
-  #     message: 'Invalid login credentials',
-  #     status: 401
-  #   }, status: :unprocessable_entity
-  # end
-
-  # def respond_to_on_destroy
-  #   log_out_success && return if current_user
-
-  #   log_out_failure
-  # end
-
-  # def log_out_success
-  #   render json: {
-  #     message: 'You are logged out',
-  #     status: 204
-  #   }, status: :ok
-  # end
-
-  # def log_out_failure
-  #   render json: {
-  #     message: 'something went wrong'
-  #   }, status: :unauthorized
-  # end
-  # include CurrentAdminConcern
-
-  before_action :set_current_admin
-  
-
+  # private
 
 def set_current_admin
     if session[:admin_id]
@@ -58,19 +10,18 @@ def set_current_admin
     end
 end
 
-  def create
-    admin = Admin
-                .find_by(email: params["email"])
-                .try(:authenticate, params["password"])
-    if admin
+def create
+    admin = Admin.find_by(email: params[:email])
+    
+    if admin.present? 
       session[:admin_id] = admin.id
       render json:{
-        status: :created,
+        status: 200,
         sign_in: true,
         admin: admin
       }
     else
-      render json: { status: :unprocessable_entity}
+      render json: { status: 401}
     end
   end
 
